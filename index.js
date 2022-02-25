241b7c9e52b21e255f9c0dbf7b7c854d7a16d97b
JeremyDelorme <jeremydelorme23.7@gmail.com>
2022-02-25 17:10:47 -0600

    Installed morgan + reordered Express statements

--- index.js
+++ index.js
@@ -1,36 +1,77 @@
+//LOADED EXPRESS FRAMEWORK
 const express = require("express");
 const app = express();
 
-let topBooks = [
+//IMPORT MORGAN MIDDLEWARE LIBRARY
+const morgan = require("morgan");
+
+//CREATE AN ARRAY OF OBJECTS HOLDING TOP MOVIES
+let topMovies = [
   {
-    title: 'Harry Potter and the Sorcerer\'s Stone',
-    author: 'J.K. Rowling'
+    name: "Movie 1",
+    director: "Director 1"
   },
   {
-    title: 'Lord of the Rings',
-    author: 'J.R.R. Tolkien'
+    name: "Movie 2",
+    director: "Director 2"
   },
   {
-    title: 'Twilight',
-    author: 'Stephanie Meyer'
+    name: "Movie 3",
+    director: "Director 3"
+  },
+  {
+    name: "Movie 4",
+    director: "Director 4"
+  },
+  {
+    name: "Movie 5",
+    director: "Director 5"
+  },
+  {
+    name: "Movie 6",
+    director: "Director 6"
+  },
+  {
+    name: "Movie 7",
+    director: "Director 7"
+  },
+  {
+    name: "Movie 8",
+    director: "Director 8"
+  },
+  {
+    name: "Movie 9",
+    director: "Director 9"
+  },
+  {
+    name: "Movie 10",
+    director: "Director 10"
   }
 ];
 
-// GET requests
-app.get('/', (req, res) => {
-  res.send('Welcome to my book club!');
+//LOG BASIC REQUEST DATA IN TERMINAL USING MORGAN MIDDLEWARE LIBRARY
+app.use(morgan("common"));
+
+//GET topMovies JSON FOR '/movies' REQUEST URL
+app.get("/movies", (req, res) => {
+  res.json(topMovies);
 });
 
-app.get('/documentation', (req, res) => {
-  res.sendFile('public/documentation.html', { root: __dirname });
+//GET WELCOME MESSAGE FOR '/' REQUEST URL
+app.get("/", (req, res) => {
+  res.send("Welcome to the myMovies App!");
 });
 
-app.get('/books', (req, res) => {
-  res.json(topBooks);
+//SERVE STATIC CONTENT FOR THE APP FROM THE 'public' DIRECTORY
+app.use(express.static("public"));
+
+//ERROR HANDLING
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
-
-// listen for requests
+//LISTEN TO PORT 8080
 app.listen(8080, () => {
-  console.log('Your app is listening on port 8080.');
+  console.log("Your App is still listening on port 8080.");
 });
